9a61d133c4431603ab6221cb7bc4e55e87a76130
flash <flash@ip-172-31-3-10>
2022-06-15 08:36:23 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## buzztag
+
+buzztag
+
+#### License
+
+MIT
---
+++ buzztag/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "buzztag",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("buzztag")
+		}
+	]
