024d98948b3eb16cc72490ffe5d9f0534d5ad173
bizmapNikhil <nikhil.tulankar@bizmap.in>
2022-07-05 12:04:42 +0530

    [fix][user perm assign to on task]

--- buzztag/buzztag/custom_script/task.py
+++ buzztag/buzztag/custom_script/task.py
@@ -4,22 +4,43 @@ def validate(doc,method):
 	set_permissions(doc)
 
 def set_permissions(doc):
-	if not check_duplicate_permission(doc):
-		user_permission=frappe.new_doc("User Permission")
-		user_permission.applicable_for="Task"
-		user_permission.user=doc.assign_task
-		user_permission.allow="Employee"
-		user_permission.for_value=frappe.db.get_value("Employee", {'user_id':doc.assign_task}, 'name')
-		user_permission.apply_to_all_doctypes=0
-		user_permission.reference_doctype=doc.doctype
-		user_permission.reference_docname=doc.name
-		user_permission.save()
+	reports_to_list = make_reports_to_data(doc)
+	if reports_to_list:
+		for report_to in reports_to_list:
+			user = frappe.db.get_value("Employee", {'name':report_to}, 'user_id')
+			if user:
+				if not check_duplicate_permission(doc,user):
+					user_permission=frappe.new_doc("User Permission")
+					user_permission.applicable_for="Task"
+					user_permission.user=user
+					user_permission.allow="Employee"
+					user_permission.for_value=doc.assign_to_user
+					user_permission.apply_to_all_doctypes=0
+					user_permission.reference_doctype=doc.doctype
+					user_permission.reference_docname=doc.name
+					user_permission.save()
+			elif report_to:
+				frappe.msgprint("User id is missing for '{0}' assign_to_user whose report to of '{1}'".format(report_to,doc.assign_to_user))
+	else:
+		frappe.msgprint("Please add report to for '{0}' assign_to_user".format(doc.assign_to_user))
 
-def check_duplicate_permission(doc):
+def make_reports_to_data(doc):
+	reports_to_list = []
+	if doc.assign_to_user:
+		reports_to_list.append(doc.assign_to_user)
+		reports_to = frappe.db.get_value("Employee", {'name':doc.assign_to_user}, 'reports_to')
+		while reports_to != None:
+			reports_to_list.append(reports_to)
+			reports_to = frappe.db.get_value("Employee", {'name':reports_to}, 'reports_to')
+		return reports_to_list
+	else:
+		frappe.msgprint("Please add value in assign_to_user field .")
+
+def check_duplicate_permission(doc,user):
 	return  frappe.db.get_all("User Permission", filters={
 			'allow': "Employee",
-			'for_value': frappe.db.get_value("Employee", {'user_id':doc.assign_task}, 'name'),
-			'user': doc.assign_task,
+			'for_value': doc.assign_to_user,
+			'user': user,
 			'applicable_for': "Task",
 			'name': ['!=', doc.name]
 		}, limit=1)
